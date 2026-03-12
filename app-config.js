0460ef16c593129e5b4235c6018d5aa239cdfa6a
Your Name <your_github_email@example.com>
2026-03-12 14:53:17 +0400

    Add Supabase URL

---
+++ app-config.js
@@ -0,0 +1,8 @@
+window.APP_CONFIG = {
+  // Add values from Supabase Project Settings -> API
+  // Example:
+  // supabaseUrl: "https://xxxx.supabase.co",
+  // supabaseAnonKey: "eyJ..."
+  supabaseUrl: "https://xidoisbayucrbolwtouo.supabase.co",
+  supabaseAnonKey: "sb_publishable_mTvtx740DfRyUg4nNUp3gw_pIiM5rpW"
+};
